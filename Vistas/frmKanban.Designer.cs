e08ed0b73669200eae9483ced04acb4609b51474
Joana Fialho dos Santos <2241540@my.ipleiria.pt>
2025-06-02 21:47:16 +0100

    --

--- Vistas/frmKanban.Designer.cs
+++ Vistas/frmKanban.Designer.cs
@@ -53,7 +53,12 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             this.button1 = new System.Windows.Forms.Button();
+=======
+            this.buttonEditarTarefa = new System.Windows.Forms.Button();
+            this.buttonApagarTarefa = new System.Windows.Forms.Button();
+>>>>>>> Stashed changes
 =======
             this.buttonEditarTarefa = new System.Windows.Forms.Button();
             this.buttonApagarTarefa = new System.Windows.Forms.Button();
@@ -228,9 +233,12 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             this.btSetDoing.Location = new System.Drawing.Point(220, 618);
             this.btSetDoing.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -250,9 +258,12 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             this.btSetDone.Location = new System.Drawing.Point(633, 618);
             this.btSetDone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -272,9 +283,12 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             this.btSetTodo.Location = new System.Drawing.Point(430, 618);
             this.btSetTodo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -294,9 +308,12 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             this.btNova.Location = new System.Drawing.Point(20, 618);
             this.btNova.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -334,6 +351,7 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(96, 618);
@@ -345,6 +363,8 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -370,6 +390,9 @@
             this.buttonApagarTarefa.UseVisualStyleBackColor = true;
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
@@ -389,6 +412,9 @@
             this.Controls.Add(this.buttonEditarTarefa);
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
@@ -446,7 +472,12 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         private System.Windows.Forms.Button button1;
+=======
+        private System.Windows.Forms.Button buttonEditarTarefa;
+        private System.Windows.Forms.Button buttonApagarTarefa;
+>>>>>>> Stashed changes
 =======
         private System.Windows.Forms.Button buttonEditarTarefa;
         private System.Windows.Forms.Button buttonApagarTarefa;
